3ebddc1ef8309e1c09dfda72fc23bc61fbf86043
Formanj-0 <formanj@colostate.edu>
2025-04-02 22:47:27 -0600

    Ready for tomorrow I think

---
+++ SpermTracking.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1                # Request 1 GPU
+#SBATCH --exclude=gpu1              # Exclude node gpu1
+#SBATCH --partition=all             # Use the GPU partition
+#SBATCH --job-name=Image_Processing # Job name
+#SBATCH --ntasks=1                  # Number of tasks
+#SBATCH --no-requeue
+#SBATCH --exclusive                 # Limit to one job per node
+
+# module purge
+module load gnu9/9.4.0
+module load cuda/10.2
+
+echo "Starting my job..."
+
+# Start timing the process
+start_time=$(date +%s)
+
+aviPath=$1
+path_to_executable="${PWD%/*}/Sperm_Cellpose.py"
+
+# ########### PYTHON PROGRAM #############################
+# Ensure output directory exists
+mkdir -p "${PWD}/output"
+
+# Correct output file name
+output_names="${PWD}/output/output_${SLURM_JOB_ID}_$(basename ${kwarg_location})"
+
+# Activate the environment and run the script
+source ../.venv/bin/activate
+python "$path_to_executable" "$aviPath" >> "$output_names" 2>&1 &
+wait
+
+
+# End timing the process
+end_time=$(date +%s)
+total_time=$(( (end_time - start_time) / 60 ))
+
+# Print the time to complete the process
+echo "Total time to complete the job: $total_time minutes"
+
+# ########### TO EXECUTE RUN IN TERMINAL #########################
+# run as: sbatch runner_pipeline.sh /dev/null 2>&1 & disown
+
+exit 0
+
+# ########### TO REMOVE SOME FILES #########################
+
+# To remove files
+# ls *.tif
+# rm -r temp_*
+# rm -r analysis_*
+# rm -r slurm* out* temp_* masks_*
+
+# ########### SLURM COMMANDS #########################
+# scancel [jobid]
+# squeue -u [username]
+# squeue
---
+++ Sperm_Cellpose.py
@@ -0,0 +1,99 @@
+# %%
+import cv2
+import cellpose
+from cellpose import models
+import numpy as np
+from skimage.color import rgb2gray
+from skimage.measure import regionprops_table, regionprops
+import matplotlib.pyplot as plt
+import torch
+import pandas as pd
+import argparse
+import os
+
+# %%
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
+
+# %%
+
+parser = argparse.ArgumentParser(description="Process a video file for cell analysis.")
+parser.add_argument("input_video_path", type=str, help="Path to the input video file")
+args = parser.parse_args()
+
+input_video_path = args.input_video_path
+
+cap = cv2.VideoCapture(input_video_path)
+imgs = []
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret:
+        # print(frame, ret)
+        # plt.imshow(frame[:, :, 0])
+        # plt.show()
+        imgs.append(rgb2gray(frame))
+    else:
+        break
+
+cap.release()
+
+# %%
+imgs = np.stack(imgs, axis=0)
+
+# %%
+imgs.shape
+
+# %%
+cp = models.CellposeModel(gpu=True, device=torch.device(device), pretrained_model=r'Data\Videos\human_20241212_500e')
+sz = models.SizeModel(cp, device=torch.device(device))
+
+model = models.Cellpose(gpu=True, device=torch.device(device))
+model.cp = cp
+model.sz = sz
+
+# %%
+masks = np.zeros_like(imgs)
+
+for t in range(len(masks)):
+    results = model.eval(imgs[t])
+    mask, flow, style, diams = results[0], results[1][0], results[1][1], results[2]
+    masks[t] = mask.astype(int)
+
+
+# %%
+for t in range(len(masks)):
+    plt.imshow(masks[t])
+    plt.show()
+
+# %%
+values_to_measure = ['label', 'area', 'centroid', 'bbox']
+df = None
+
+
+for t in range(len(masks)):
+    results = regionprops_table(masks[t].astype(int), imgs[t], values_to_measure)
+    results['time'] = t
+    
+    if df is None:
+        df = pd.DataFrame(results)
+    else:
+        df = pd.concat([df, pd.DataFrame(results)], ignore_index=True)
+
+# %%
+df
+
+# %%
+output_csv_path = os.path.splitext(input_video_path)[0] + '_sperm_measurements.csv'
+df.to_csv(output_csv_path)
+
+# %%
+
+
+
